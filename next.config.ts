eaa59d4e23ae776c2939db0378addf3b325b8b85
Minsu Kang <powerful@postech.ac.kr>
2024-12-30 21:43:17 +0900

    webpack option 추가 (turbopack은 dev만 지원함)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {
